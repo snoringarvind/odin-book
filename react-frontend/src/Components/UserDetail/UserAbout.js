a59199d9c5e1098c3528c73ba7010d4b2d79590f
snoringarvind <snoringarvind@gmail.com>
2021-02-15 09:26:46 +0530

    saving

--- react-frontend/src/Components/UserDetail/UserAbout.js
+++ react-frontend/src/Components/UserDetail/UserAbout.js
@@ -2,32 +2,16 @@ import React, { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { OdinBookContext } from "../Context";
 import { axios_request } from "../Utils";
+import UseraAboutCard from "./UserAboutCard";
+import uniqid from "uniqid";
+import "./UserAbout.css";
 
 const UserAbout = () => {
   const { myAboutValue, didMyAboutMountValue } = useContext(OdinBookContext);
   const [myAbout, setMyAbout] = myAboutValue;
   const [didMyAboutMount, setdidMyAboutMount] = didMyAboutMountValue;
 
-  const [state, setState] = useState({
-    fname: "",
-    lname: "",
-    bio: "",
-    nickName: "",
-    school: "",
-    college: "",
-    working: "",
-    relationshipStatus: "",
-    book: "",
-    food: "",
-
-    gender: "",
-    dob: "",
-  });
-
-  const changeHandler = (e) => {
-    const { name, value } = e.target;
-    setState({ ...state, [name]: value });
-  };
+  const [result, setResult] = useState();
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -54,8 +38,6 @@ const UserAbout = () => {
 
   const [getLoading, setGetLoading] = useState(true);
 
-  const [result, setResult] = useState({});
-
   const [about, setAbout] = useState("");
   const [error, setError] = useState("");
 
@@ -68,13 +50,14 @@ const UserAbout = () => {
   const profile_route = `/profile/${userid}`;
   const profile_route_method = "GET";
 
-  const [click, setClick] = useState(false);
+  const [clickIndex, setClickIndex] = useState(null);
 
   const make_server_request = () => {
     const cb_response = (response) => {
       if (userid === jwtData.sub) {
         setMyAbout(response.data);
       }
+      console.log(response.data);
       setResult(response.data);
 
       setGetLoading(false);
@@ -110,208 +93,39 @@ const UserAbout = () => {
     }
   }, []);
 
-  const display_about = () => {
-    return (
-      <>
-        <div>
-          <span className="title">Name:</span> <span>{result.fname} </span>
-          <span>{result.lname}</span>
-        </div>
-        {result.bio && (
-          <div>
-            <span className="title">Bio:</span>
-            {result.bio}
-          </div>
-        )}
-        {result.nickName && (
-          <div>
-            <span className="titie">NickName:</span>
-            {result.nickName}
-          </div>
-        )}
-        {result.school && (
-          <div>
-            <span className="title">School:</span>
-            {result.school}
-          </div>
-        )}
-        {result.college && (
-          <div>
-            <span className="title">College:</span>
-            {result.college}
-          </div>
-        )}
-        {result.working && (
-          <div>
-            <span className="title">Work:</span>
-            {result.working}
-          </div>
-        )}
-        {result.relationshipStatus && (
-          <div>
-            <span className="title">Relationship Status:</span>
-            {result.relationshipStatus}
-          </div>
-        )}
-        {result.book && (
-          <div>
-            <span className="title">Books:</span>
-            {result.book}
-          </div>
-        )}
-        {result.food && (
-          <div>
-            <span className="title">Food:</span>
-            {result.food}
-          </div>
-        )}
-        {result.phone && (
-          <div>
-            <span className="title">Phone:</span>
-            {result.phone}
-          </div>
-        )}
-        {result.email && (
-          <div>
-            <span className="title">Email:</span>
-            {result.email}
-          </div>
-        )}
-        {result.gender && (
-          <div>
-            <span className="title">Gender:</span>
-            {result.gender}
-          </div>
-        )}
-        {result.dob && (
-          <div>
-            <span className="title">Birthday</span>
-            {result.dob}
-          </div>
-        )}
-      </>
-    );
-  };
+  let g;
+  if (result) {
+    delete result.user;
+    delete result._id;
+    delete result.__v;
 
-  const display_myabout = () => {
-    return (
-      <div>
-        <form>
-          <div className="form-group">
-            <label htmlFor="fname">First Name:</label>
-            <input
-              name="fname"
-              id="fname"
-              placeholder="Your first name"
-              value={result.fname}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lname">Last Name:</label>
-            <input
-              name="lname"
-              id="lname"
-              placeholder="Your last name"
-              value={result.lname}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="bio">Bio:</label>
-            <input
-              name="bio"
-              placeholder="Enter your bio"
-              value={result.bio}
-              id="bio"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="nickName">NickName"</label>
-            <input
-              name="nickName"
-              placeholder="Enter your nick-name"
-              id="nickName"
-              value={result.nickName}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="school">School:</label>
-            <input
-              name="school"
-              placeholder="Enter your School name here"
-              id="school"
-              value={result.school}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="college">College:</label>
-            <input
-              name="college"
-              placeholder="Enter your college here"
-              id="college"
-              value={result.college}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="working">Work Place:</label>
-            <input
-              name="working"
-              placeholder="Enter tour work place here"
-              id="working"
-              value={result.working}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="relationshipStatus">
-              Choose your relationship-status:
-            </label>
-            <select id="relationshipStatus" name="relationshipStatus">
-              <option value="status">Status</option>
-              <option value="married">Married</option>
-              <option value="single">Single</option>
-              <option value="divorced">Divorced</option>
-              <option value="complicated">It's complicated</option>
-              <option value="in-relationship">In relationship</option>
-              <option value="open-relationship">Open Relationship</option>
-              <option value="engaged">Engaged</option>
-              <option value="widowed">Widowed</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="book">Book:</label>
-            <input
-              id="book"
-              placeholder="Enter books you like"
-              name="book"
-              value={result.book}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="food">Food:</label>
-            <input
-              id="food"
-              placeholder="Add food"
-              name="food"
-              value={result.food}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="gender">Gender:</label>
-            <select id="gender" name="gender">
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="not-say">Not say</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="dob" name="dob">
-              Date of birth
-            </label>
-            <input name="dob" value={result.dob} type="date" id="dob" />
-          </div>
-        </form>
-      </div>
-    );
-  };
+    g = Object.keys(result);
+    console.log(g);
+  }
+
+  const [ee, setee] = useState({});
+
+  useEffect(() => {
+    if (result) {
+      setee({
+        fname: result.fname,
+        lname: result.lname,
+        bio: result.bio,
+        nickName: result.nickName,
+        school: result.school,
+        college: result.college,
+        working: result.working,
+        relationshipStatus: result.relationshipStatus,
+        book: result.book,
+        food: result.food,
+
+        gender: result.gender,
+        dob: result.dob,
+      });
+    }
+  }, [result]);
+
+  // const [ee, setee] = useState();
 
   return (
     <div className="UserAbout">
@@ -319,10 +133,22 @@ const UserAbout = () => {
       {!getLoading &&
         (error ? (
           <div className="error">{error}</div>
-        ) : jwtData.sub == userid ? (
-          display_myabout()
         ) : (
-          display_about()
+          g.map((value, index) => {
+            return (
+              <UseraAboutCard
+                key={uniqid()}
+                objkey={value}
+                index={index}
+                clickIndex={clickIndex}
+                setClickIndex={setClickIndex}
+                result={result}
+                setResult={setResult}
+                ee={ee}
+                setee={setee}
+              />
+            );
+          })
         ))}
     </div>
   );
--- react-frontend/src/Components/UserDetail/UserAboutCard.js
+++ react-frontend/src/Components/UserDetail/UserAboutCard.js
@@ -1,7 +1,118 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { OdinBookContext } from "../Context";
 
-const UseraAboutCard = () => {
-  return <div className="UserAboutCard"></div>;
+const UseraAboutCard = ({
+  objkey,
+  index,
+  clickIndex,
+  setClickIndex,
+  result,
+  setResult,
+  ee,
+  setee,
+}) => {
+  const { myAboutValue, didMyAboutMountValue } = useContext(OdinBookContext);
+  const [myAbout, setMyAbout] = myAboutValue;
+
+  const [state, setState] = useState({
+    fname: result[objkey],
+    lname: result[objkey],
+    bio: result[objkey],
+    nickName: result[objkey],
+    school: result[objkey],
+    college: result[objkey],
+    working: result[objkey],
+    relationshipStatus: result[objkey],
+    book: result[objkey],
+    food: result[objkey],
+
+    gender: result[objkey],
+    dob: result[objkey],
+  });
+  const str = {
+    fname: "First Name",
+    lname: "Last Name",
+    bio: "Bio",
+    nickName: "Nick-name",
+    school: "School",
+    college: "College",
+    working: "Work Place",
+    relationshipStatus: "Relationhsip Status",
+    book: "Book",
+    food: "Food",
+    gender: "Gender",
+    dob: "Date of birth",
+  };
+  const changeHandler = (e) => {
+    // console.log(e.key);
+    e.preventDefault();
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+  useEffect(() => {
+    const element = document.querySelector(`#${objkey}`);
+    if (element) {
+      element.addEventListener("keydown", (e) => {
+        // console.log(e.key, "jdfjdfjdfnjdfn");
+        if (e.key == "Escape") {
+          setClickIndex(null);
+        }
+      });
+    }
+  }, []);
+
+  console.log(state[objkey]);
+  return (
+    <div className="UserAboutCard">
+      <form>
+        <div className="form-group">
+          <label htmlFor={objkey}>{str[objkey]}</label>
+          <div className="container">
+            {clickIndex != index && (
+              <div className="profile-values">{ee[objkey]}</div>
+            )}
+            {clickIndex == index && (
+              <input
+                name={objkey}
+                id={objkey}
+                value={state[objkey]}
+                onChange={changeHandler}
+              />
+            )}
+          </div>
+          {clickIndex !== index && (
+            <div
+              className="edit-icon fas fa-edit"
+              onClick={(e) => {
+                console.log(e);
+                e.preventDefault();
+                e.stopPropagation();
+                setClickIndex(index);
+              }}
+            ></div>
+          )}
+          {clickIndex == index && (
+            <div
+              className="save-icon fas fa-save"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setClickIndex(null);
+                // result.objkey = state.objkey;
+                // console.log(result);
+                // setResult(result);
+                ee[objkey] = state[objkey];
+                setee(ee);
+                myAbout[objkey] = state[objkey];
+                setMyAbout(myAbout);
+              }}
+            ></div>
+          )}
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default UseraAboutCard;

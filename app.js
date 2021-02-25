5e3367352808b6a88e7d8a3c45ac4c5f7ca414b9
snoringarvind <snoringarvind@gmail.com>
2021-02-25 19:24:16 +0530

    saving

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ const odinbookRouter = require("./routes/odinbook");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(cors({ origin: "https://snoringarvind.github.io" }));
-app.use(logger("dev"));
+app.use(cors({ origin: "http://localhost:3000" }));
+
+http: http: app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
